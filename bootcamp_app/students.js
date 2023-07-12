1c42e299c5a1cf023d3bcee4d8719c515d2bf03c
swathij943 <swathij943@gmail.com>
2023-07-11 20:11:35 -0400

    Implemented parametized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -11,12 +11,17 @@ const params = {
 const pool = new Pool(params);
 pool.connect();
 
-pool.query(`
+const queryString = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort_name
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;`
+const cohortName = process.argv[2]
+const limit = process.argv[3] || 5
+const values = [`%${cohortName}%`, limit]
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`)
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,25 +1,28 @@
 const { Pool } = require('pg')
-
 const params = {
   port: '5432',
-  user: 'labber',
+  user: 'vagrant',
   password: '123',
   host: 'localhost',
   database: 'bootcampx'
 }
-
 const pool = new Pool(params)
 pool.connect 
 
-pool.query(`
+const query = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests 
 JOIN students ON students.id = student_id 
 JOIN cohorts ON cohorts.id = cohort_id 
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teacher, cohort
-ORDER BY teacher;`)
+ORDER BY teacher;`
+
+const cohortName = process.argv[2]
+const values = [`%${cohortName}%`]
+
+pool.query(query, values)
     .then(res => {
       res.rows.forEach(row => {
         console.log(`${row.cohort}: ${row.teacher}`);
